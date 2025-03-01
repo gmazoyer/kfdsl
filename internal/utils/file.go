e9a5134117cc402218af2ff02e2b54ab78ee1e3a
Guillaume Mazoyer <guillaume@mazoyer.eu>
2025-03-01 12:57:01 +0100

    Add dependency installer
    This version allows to install dependencies defined in a JSON formatted
    file. Dependencies are installed one by one. Each of them are fetch from
    a remote file and installed locally given a path. A checksum can be
    optionaly defined to ensure that the proper files are installed. This
    will also force them to be re-installed if checksums do not match.
    
    Right now, dependencies are fetched and installed in a linear fashion.
    We may improve this by introducing some parallelism in a near future.

--- internal/utils/file.go
+++ internal/utils/file.go
@@ -2,7 +2,13 @@ package utils
 
 import (
 	"archive/zip"
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
 	"fmt"
+	"hash"
 	"io"
 	"net/http"
 	"os"
@@ -10,32 +16,98 @@ import (
 	"strings"
 )
 
-func CopyFile(srcPath, destPath string) error {
-	sourceFile, err := os.Open(srcPath)
-	if err != nil {
-		return err
+func FileChecksum(file *os.File, checksumType string) (string, error) {
+	var hasher hash.Hash
+	switch checksumType {
+	case "sha256":
+		hasher = sha256.New()
+	case "sha512":
+		hasher = sha512.New()
+	case "sha1":
+		hasher = sha1.New()
+	case "md5":
+		hasher = md5.New()
+	default:
+		return "", fmt.Errorf("unknown checksum type %s", checksumType)
 	}
-	defer sourceFile.Close()
 
-	destinationFile, err := os.Create(destPath)
-	if err != nil {
-		return err
+	file.Seek(0, 0)
+	if _, err := io.Copy(hasher, file); err != nil {
+		return "", err
 	}
-	defer destinationFile.Close()
 
-	_, err = io.Copy(destinationFile, sourceFile)
-	if err != nil {
-		return err
-	}
-	return destinationFile.Sync()
+	return hex.EncodeToString(hasher.Sum(nil)), nil
 }
 
-func MoveFile(srcPath, dstPath string) error {
-	if err := CopyFile(srcPath, dstPath); err != nil {
+func FileMatchesChecksum(filename, checksum string) (bool, error) {
+	// Expects: checksum_type:checksum_string
+	checksumDetails := strings.Split(checksum, ":")
+	if len(checksumDetails) != 2 {
+		return false, fmt.Errorf("invalid checksum format: %s", checksum)
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return false, fmt.Errorf("failed to open source file: %w", err)
+	}
+
+	hash, err := FileChecksum(file, checksumDetails[0])
+	if err != nil {
+		return false, fmt.Errorf("failed to compute checksum for %s: %w", filename, err)
+	}
+
+	return hash == checksumDetails[1], nil
+}
+
+func FileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil || !os.IsNotExist(err)
+}
+
+func FileExistsAndMatchesChecksum(filename, checksum string) (bool, error) {
+	if !FileExists(filename) {
+		return false, nil
+	}
+	if checksum == "" {
+		return true, nil
+	}
+	return FileMatchesChecksum(filename, checksum)
+}
+
+func CopyFile(srcPath, destPath string) error {
+	srcFile, err := os.Open(srcPath)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	destFile, err := os.Create(destPath)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, srcFile)
+	if err != nil {
+		return err
+	}
+	return destFile.Sync()
+}
+
+func MoveFile(srcPath, dstPath, checksum string) error {
+	exists, err := FileExistsAndMatchesChecksum(dstPath, checksum)
+	if err != nil {
+		return fmt.Errorf("failed to check destination file for existance and checksum: %w", err)
+	}
+	if exists {
+		return nil
+	}
+
+	if err = CopyFile(srcPath, dstPath); err != nil {
 		return fmt.Errorf("failed to copy file: %w", err)
 	}
 
-	if err := os.Remove(srcPath); err != nil {
+	if err = os.Remove(srcPath); err != nil {
 		return fmt.Errorf("failed to remove source file: %w", err)
 	}
 
@@ -60,7 +132,7 @@ func CopyAndReplaceFile(srcPath, destPath string) error {
 		return fmt.Errorf("failed to copy file: %w", err)
 	}
 
-	srcInfo, err := os.Stat(srcPath)
+	srcInfo, err := srcFile.Stat()
 	if err != nil {
 		return fmt.Errorf("failed to get source file info: %w", err)
 	}
@@ -72,12 +144,17 @@ func CopyAndReplaceFile(srcPath, destPath string) error {
 	return nil
 }
 
-func FileExists(filename string) bool {
-	_, err := os.Stat(filename)
-	return err == nil || !os.IsNotExist(err)
+func CreateDirIfNotExists(parts ...string) (string, error) {
+	path := filepath.Join(parts...)
+
+	if err := os.MkdirAll(path, os.ModePerm); err != nil {
+		return "", fmt.Errorf("failed to create directory %s: %w", path, err)
+	}
+
+	return path, nil
 }
 
-func DownloadFile(url string) (string, error) {
+func DownloadFile(url, checksum string) (string, error) {
 	filename := filepath.Join(os.TempDir(), url[strings.LastIndex(url, "/")+1:])
 
 	out, err := os.Create(filename)
@@ -93,6 +170,23 @@ func DownloadFile(url string) (string, error) {
 	defer response.Body.Close()
 
 	_, err = io.Copy(out, response.Body)
+	if err != nil {
+		return filename, fmt.Errorf("failed to write file %s on disk: %w", filename, err)
+	}
+	err = out.Sync()
+	if err != nil {
+		return filename, fmt.Errorf("failed to sync file %s on disk: %w", filename, err)
+	}
+
+	if checksum != "" {
+		match, err := FileMatchesChecksum(filename, checksum)
+		if err != nil {
+			return filename, err
+		}
+		if !match {
+			return filename, fmt.Errorf("file %s does not match checksum %s", filename, checksum)
+		}
+	}
 
 	return filename, err
 }
